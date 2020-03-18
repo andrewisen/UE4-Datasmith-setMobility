050dc3892b91c4f4daaf19b5cb7a11061e36e24f
André "MG" Wisén <andre.wisen@gmail.com>
2020-03-18 10:17:27 +0100

    Add upload form

--- webb_helper/index.php
+++ webb_helper/index.php
@@ -2,10 +2,12 @@
 <html>
 <body>
 
-<?php
-	echo "Hello GitHub";
-?>
+<form action="./uploadDatasmithFile.php" method="post" enctype="multipart/form-data">
+    Select an .udatasmith file to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <br>
+    <input type="submit" value="Upload" name="submit">
+</form>
 
 </body>
 </html>
-
